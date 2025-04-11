9f1fe038cd216a273517c321891689a0c3ee89a5
mlnls <tmdals140@naver.com>
2025-04-11 20:55:09 +0900

    [FEAT] MainImage Component 제작
    feat: #3

---
+++ src/shared/component/MainImage/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MainImage() {
+  return <div className="w-[405px] h-[405px] bg-stone-500"></div>;
+}
--- src/shared/component/MainView/index.tsx
+++ src/shared/component/MainView/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import WelcomeMessage from "@/shared/component/MainView/WelcomeMessage";
 import TodayFocusTime from "@/shared/component/MainView/TodayFocusTime";
-import ThisWeekGroup from "./ThisWeekGroup";
+import ThisWeekGroup from "@/shared/component/MainView/ThisWeekGroup";
+import MainImage from "@/shared/component/MainImage";
 
 export default function StudyOverview() {
   return (
@@ -10,8 +11,9 @@ export default function StudyOverview() {
         <WelcomeMessage />
         <TodayFocusTime />
       </div>
-      <div className="mx-auto ml-[75px] mr-[90px] mt-[120px] space-y-6 text-gray-800 flex justify-between">
+      <div className="mx-auto ml-[75px] mr-[90px] mt-[200px] space-y-6 text-gray-800 flex justify-between">
         <ThisWeekGroup />
+        <MainImage />
       </div>
     </>
   );
