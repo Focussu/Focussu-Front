e3bafdba33e9fea87c82e9f90176eb2809b14bbb
mlnls <tmdals140@naver.com>
2025-04-12 18:11:17 +0900

    [FEAT] SearchBar Component 제작 및 배치
    feat: #8

---
+++ src/shared/component/SearchBar/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+export default function SearchBar() {
+  return (
+    <div className="w-[460px] h-[36px] border-[2px] border-gray-400 rounded-4xl flex justify-between">
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요"
+        className="border-none text-[14px] w-[400px] ml-[25px]"
+      />
+      <Image
+        className="dark:invert mr-[17px]"
+        src="/searchIcon.svg"
+        alt="SearchIcon"
+        width={20}
+        height={20}
+        priority
+      />
+    </div>
+  );
+}
