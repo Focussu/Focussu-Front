b304d9193a88c4113f806b0df77fbe58ee07bee9
mlnls <tmdals140@naver.com>
2025-04-14 04:40:42 +0900

    [FEAT] StudyRoom Footer Component 제작 및 배치
    feat: #12

--- src/shared/component/RoomFooter/index.tsx
+++ src/shared/component/RoomFooter/index.tsx
@@ -48,7 +48,26 @@ export default function RoomFooter() {
           )}
         </div>
       </div>
-      <div className="flex-1 h-full"></div>
+      <div className="flex-1 h-full mr-[30px] flex justify-end items-center">
+        <div className="w-[380px] h-[90px] border-[1.5] bg-white flex flex-col rounded-lg">
+          <div className="flex flex-row gap-[20px] ml-[12px] mt-[7px]">
+            <div className="text-[15px] font-bold">오승민</div>
+            <div className="w-[80px] h-[22px] text-[10px] rounded-lg border-1 border-gray-400 justify-center flex items-center ">
+              ✅ 현재 활동 중
+            </div>
+          </div>
+          <div className="flex h-full mx-[10px] mt-[5px]">
+            <div className="flex-1 flex flex-col gap-[5px]">
+              <div className="text-[13px]">총 집중시간 : 10293h 29m 31s</div>
+              <div className="text-[13px]">금주 집중시간 : 12h 30m 20s</div>
+            </div>
+            <div className="flex-1 flex flex-col gap-[5px] ml-[20px]">
+              <div className="text-[13px]">오늘 집중시간 : 2h 30m 20s</div>
+              <div className="text-[13px]">현재 집중도 : 87점</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
