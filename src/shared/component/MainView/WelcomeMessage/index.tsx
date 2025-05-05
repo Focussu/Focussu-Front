fdf7be4cff22fbf2806b2aad471c1d024cb9de0f
mlnls <tmdals140@naver.com>
2025-05-05 17:51:54 +0900

    [CHORE] WelcomeMessage Component 반응형
    chore: #18

--- src/shared/component/MainView/WelcomeMessage/index.tsx
+++ src/shared/component/MainView/WelcomeMessage/index.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 export default function WelcomeMessage() {
   return (
-    <div className="mt-[135px] flex flex-col">
-      <h1 className="text-[64px] text-#000000 font-bold mb-1 flex justify-center">
+    <div className="flex flex-col items-center text-center justify-center">
+      <h1 className="text-[clamp(1.5rem,4vw,3rem)] font-bold mb-2 text-black whitespace-nowrap">
         00님 반가워요!
       </h1>
-      <p className="text-[24px] text-#000000 text-zinc-500 flex justify-center">
+      <p className="text-[clamp(1rem,2.5vw,1.25rem)] text-zinc-500 whitespace-nowrap">
         1D 12H 3M 만에 들어오셨네요!
       </p>
     </div>
