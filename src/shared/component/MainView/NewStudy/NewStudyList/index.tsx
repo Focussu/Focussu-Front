3c36eff3e0e54e80015cd6ab8faa57c66d8bfb6a
mlnls <tmdals140@naver.com>
2025-05-05 19:21:09 +0900

    [CHORE] CurrentStudy / NewRecommend Component 반응형 및 mainpage에 적용
    chore: #18

--- src/shared/component/MainView/NewStudy/NewStudyList/index.tsx
+++ src/shared/component/MainView/NewStudy/NewStudyList/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function NewStudyList() {
   return (
-    <div className="w-[740px] flex flex-col text-[24px] gap-[10px]">
+    <div className="w-full max-w-[clamp(320px,50vw,740px)] flex flex-col text-[clamp(16px,1.5vw,24px)] gap-[clamp(4px,1vw,10px)]">
       <div className="text-black font-semibold">CS 마스터가 되어보자</div>
-      <div className="text-gray-400">
+      <div className="text-gray-400 truncate">
         CS 먹는 연습 아니구요, 정말 Computer Sc...
       </div>
-      <div className="text-gray-500 mb-[48px]">총 인원 : 2/40</div>
+      <div className="text-gray-500 mb-[clamp(24px,4vw,48px)]">
+        총 인원 : 2/40
+      </div>
     </div>
   );
 }
