6ff7988501b443a0987477cd0de241bea03383b5
mlnls <tmdals140@naver.com>
2025-05-11 18:47:37 +0900

    [CHORE] Mypage 반응형 기본 틀 잡기
    chore: #18

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -7,19 +7,29 @@ import Ranking from "@/shared/component/Ranking";
 
 export default function MyPage() {
   return (
-    <div className="flex flex-col lg:flex-row bg-[#E6E6E6]">
-      <div className="w-full lg:w-1/3 flex flex-col justify-center items-center pt-6 pb-6 gap-6">
-        <UserProfile />
-        <ParticipateStudy />
+    <div className="h-[calc(100vh-121px)] w-full flex flex-col lg:flex-row bg-[#E6E6E6]">
+      <div className="flex flex-col w-full lg:w-1/3 p-10 gap-10 overflow-hidden">
+        <div className="flex flex-col bg-white rounded-xl p-4 h-1/3">
+          <UserProfile />
+        </div>
+        <div className="flex flex-col bg-white rounded-xl p-4 h-2/3">
+          <ParticipateStudy />
+        </div>
       </div>
 
-      <div className="w-full lg:w-1/3 flex justify-center items-center pt-6 pb-6">
-        <PersonalAnalysis />
+      <div className="flex flex-col w-full lg:w-1/3 p-10 overflow-hidden">
+        <div className="flex flex-col bg-white rounded-xl p-4 h-full">
+          <PersonalAnalysis />
+        </div>
       </div>
 
-      <div className="w-full lg:w-1/3 flex flex-col justify-center items-center pt-6 pb-6 gap-6">
-        <FriendLists />
-        <Ranking />
+      <div className="flex flex-col w-full lg:w-1/3 p-10 gap-10 overflow-hidden">
+        <div className="flex flex-col bg-white rounded-xl p-4 h-1/2">
+          <FriendLists />
+        </div>
+        <div className="flex flex-col bg-white rounded-xl p-4 h-1/2">
+          <Ranking />
+        </div>
       </div>
     </div>
   );
