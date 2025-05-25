66aa786eb8d2763b2358448de75ececc5e6e0a54
mlnls <tmdals140@naver.com>
2025-05-25 17:05:06 +0900

    [CHORE] 회원가입 컴포넌트 반응형
    chore: #22

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import SignUp from "@/shared/component/SignUp";
 export default function SignupPage() {
   return (
     <div className="h-[calc(100vh-121px)] w-full flex items-center justify-center lg:flex-row bg-[#E6E6E6]">
-      <div className="w-[480px] h-[660px] bg-white rounded-lg flex flex-col">
+      <div className="w-full max-w-[600px] bg-white rounded-xl shadow-md p-8">
         <SignUp />
       </div>
     </div>
