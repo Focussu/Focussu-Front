9713f6589abf25f6b16522342e0d6ea0be08716c
mlnls <tmdals140@naver.com>
2025-06-01 15:42:42 +0900

    chore: @mediapipe/tasks-vision install
    chore: #26

--- src/shared/hook/function/useWebRTCManager.ts
+++ src/shared/hook/function/useWebRTCManager.ts
@@ -204,7 +204,6 @@ export function useWebRTCManager(
     setRemoteStreams([]);
     localStream?.getTracks().forEach((t) => t.stop());
     socketRef.current?.close();
-    console.log(socketRef);
   };
 
   return {
