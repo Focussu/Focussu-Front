d37f086e3982fe140597dc41ef06db9c6b6e2fd2
mlnls <tmdals140@naver.com>
2025-06-03 12:52:08 +0900

    찐최종 커밋

--- src/shared/component/MainView/CurrentStudy/CurrentStudyList/index.tsx
+++ src/shared/component/MainView/CurrentStudy/CurrentStudyList/index.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { HitSuccessStudyRoomResponse } from "@/shared/type/forAPI/RoomType";
 
-export default function CurrentStudyList() {
+type Props = {
+  room: HitSuccessStudyRoomResponse;
+};
+
+export default function CurrentStudyList({ room }: Props) {
+  const truncateDescription = (desc: string) => {
+    return desc.length > 20 ? desc.slice(0, 20) + "..." : desc;
+  };
+
   return (
     <div className="w-full max-w-[clamp(320px,50vw,740px)] flex flex-col text-[clamp(16px,1.5vw,24px)] gap-[clamp(4px,1vw,10px)]">
-      <div className="text-black font-semibold">UMC Workbook Study</div>
+      <div className="text-black font-semibold">{room.name}</div>
       <div className="text-gray-400 truncate">
-        이번 8기 진행하시는 분들 급하게 구합니다 ㅜ...
+        {truncateDescription(room.description)}
       </div>
       <div className="text-gray-500 mb-[clamp(24px,4vw,48px)]">
-        총 인원 : 1/30
+        정원 : {room.maxCapacity}
       </div>
     </div>
   );
--- src/shared/component/MainView/NewStudy/index.tsx
+++ src/shared/component/MainView/NewStudy/index.tsx
@@ -1,15 +1,40 @@
+"use client";
+
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+
 import NewStudyList from "./NewStudyList";
+import { CallAllStudyRoom } from "@/shared/hook/api/useStudyRoom";
+import {
+  CallAllStudyRoomResponse,
+  HitSuccessStudyRoomResponse,
+} from "@/shared/type/forAPI/RoomType";
 
 export default function NewStudyRecommend() {
+  const { data: allRoom } = useQuery<CallAllStudyRoomResponse>({
+    queryKey: ["AllStudyRooms"],
+    queryFn: () => CallAllStudyRoom(),
+    staleTime: 5 * 1000,
+  });
+
+  const getRandomRooms = (
+    rooms: HitSuccessStudyRoomResponse[],
+    count: number
+  ) => {
+    const shuffled = [...rooms].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+  };
+
+  const randomRooms = allRoom ? getRandomRooms(allRoom, 3) : [];
+
   return (
     <div className="flex-1 min-w-[clamp(300px,40vw,740px)]">
       <div className="text-[clamp(24px,3vw,48px)] mb-[clamp(24px,4vw,48px)] font-bold">
         새로운 스터디 찾기 🔎
       </div>
-      <NewStudyList />
-      <NewStudyList />
-      <NewStudyList />
+      {randomRooms.map((room) => (
+        <NewStudyList key={room.id} room={room} />
+      ))}
     </div>
   );
 }
